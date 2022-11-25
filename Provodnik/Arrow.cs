059d429c43f4704c4d064d5b7a7d40c2f4663197
3vgen <evgeniy.zhenia@gmail.com>
2022-11-25 14:56:55 +0300

    make structure

---
+++ Provodnik/Arrow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Provodnik
+{
+    public class Arrow
+    {
+        public int min;
+        public int max;
+        public Arrow()
+        {
+            
+        }
+    }
+}
